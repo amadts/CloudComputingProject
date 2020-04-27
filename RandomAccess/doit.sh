f66e5957affdafcfdeaa174f35e2aa31477c743d
hjlehr <hjlehr11@comcast.net>
2020-04-27 12:24:38 -0400

    Update doit.sh

--- RandomAccess/doit.sh
+++ RandomAccess/doit.sh
@@ -2,12 +2,12 @@
 
 numsockets=2
 
-for ((i=0;i<10;i++)); do ./linux.sh $numsockets; done
-for ((i=0;i<10;i++)); do ./docker.sh $numsockets; done
-for ((i=0;i<10;i++)); do ./vm.sh $numsockets; done
+#for ((i=0;i<10;i++)); do ./linux.sh $numsockets; done
+for ((i=0;i<20;i++)); do ./docker.sh $numsockets; done
+#for ((i=0;i<10;i++)); do ./vm.sh $numsockets; done
 
 numsockets=1
 
-for ((i=0;i<10;i++)); do ./linux.sh $numsockets; done
-for ((i=0;i<10;i++)); do ./docker.sh $numsockets; done
-for ((i=0;i<10;i++)); do ./vm.sh $numsockets; done
+#for ((i=0;i<10;i++)); do ./linux.sh $numsockets; done
+for ((i=0;i<20;i++)); do ./docker.sh $numsockets; done
+#for ((i=0;i<10;i++)); do ./vm.sh $numsockets; done
